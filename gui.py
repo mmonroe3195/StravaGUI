f5085625cd69d0ae591ad971de06f99259ba0051
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2022-01-19 09:33:58 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,16 +2,32 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__() #calling parent constructor
+        self.setGeometry(200, 200, 300, 300) #xpos, ypos, width, height (xpos is where screen shows up)
+        self.setWindowTitle("Strava Viewer")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Strava View")
+        self.label.move(50,50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("Clicked it here!!!!!!!!!!!!!!")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300) #xpos, ypos, width, height (xpos is where screen shows up)
-    win.setWindowTitle("Strava Viewer")
-
-    label = QtWidgets.QLabel(win)
-    label.setText("Strava View")
-    label.move(50,50)
-
+    win = MyWindow()
     win.show()
     sys.exit(app.exec_()) #so it exits properly
 
